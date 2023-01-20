848a1964a0b99d0ea4738d6972f2fd80a3af7ce7
JARVIS207207 <mbow.mouhamadou-mourtala@ugb.edu.sn>
2023-01-20 00:33:36 +0000

    Visual-paradigm

--- BDR/cours_1.sql
+++ BDR/cours_1.sql
@@ -1,5 +1,40 @@
 --mysql -u root -q 
-
--- create table personne
+-- Accorder des privileges a ipsl directeur
+grant all privileges on  *.* to ipsldirecteur@localhost identified by 'jarvis';
+--
+create database ipsl2023;
 
 create table personne (id_personn int, nom varchar(50));
+-------------------------------------------------------
+-- Connect as the user root
+mysql -u root -p
+
+-- Create a database as ipsl2023 name's 
+create database ipsl2023 ;
+
+-- Create a user name ipsldirecteur password ipsl2022@ and give them all privileges on all database
+grant all privileges on *.* to ipsldirecteur@localhost identified by 'jarvis' ;
+
+-- Create a new user ipslEtudiant as password ipslEtudiant@ and give the all privileges on ipsl2023 only 
+grant all privileges on ipsl2023.* to ipsletudiant@localhost identified by 'jarvis207 ;
+
+-- Crate a database as ipslSupermarche name's
+create database ipslSupermarche ;
+
+-- Connect as the user ipsldirecteur
+mysql -u ipsldirecteur -p
+
+-- Create Table Personne in our Supermarche Information System's
+create table personne (nom varchar(50), prenom varchar(50), mail varchar(50), telephone varchar(50), age smallint(6), id_personn varchar(10) );
+
+-- Create Table Article in our Supermarche Information System's
+create table article (id_article varchar(50) primary key, nom_article varchar(50), prix_article smallint(10), date_prod date, date_exp date, details varchar(50) ) ;
+
+-- Create Table Gerant in our Supermarche Information System's
+create table gerant (id_gerant varchar(50) primary key, experience varchar(50), specialite varchar(50), type_de_contrat varchar(50));
+
+-- Create Table  agent-securite in our Supermarche Information System's
+create table agent_securite(id_agent_sec varchar(50) primary key, grade varchar(50), experience varchar(50), heures_de_services  ) ;
+
+-- Create all tables of the our Supermarche System in ipslSupermarche
+create table 
