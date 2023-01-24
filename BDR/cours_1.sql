1859ca9581e20b90adf401cb3ed69fefce6138db
JARVIS207207 <mbow.mouhamadou-mourtala@ugb.edu.sn>
2023-01-24 18:55:51 +0000

    done

--- BDR/cours_1.sql
+++ BDR/cours_1.sql
@@ -20,17 +20,16 @@ mysql -u ipsldirecteur -p
 
 -- Create Table Personne in our Supermarche Information System's
 create table personne (
-    id_personne varchar(50) primary key ,
+    id_personne int(10) primary key auto_increment,
     nom varchar(50),
     prenom varchar(50),
     mail varchar(50),
     telephone varchar(50),
-    age smallint(6),
-    id_personn varchar(10));
+    age smallint(6));
 
 -- Create Table Article in our Supermarche Information System's
 create table article (
-    id_article varchar(50) primary key,
+    id_article int(10) primary key auto_increment,
     nom_article varchar(50),
     prix_article smallint(10),
     date_prod date,
@@ -39,54 +38,55 @@ create table article (
 
 -- Create Table Gerant in our Supermarche Information System's
 create table gerant(
-    id_gerant varchar(50) primary key,
+    id_gerant int(10) primary key auto_increment,
     experience varchar(50),
     specialite varchar(50),
     type_de_contrat varchar(50));
 
 -- Create Table  agent-securite in our Supermarche Information System's
 create table agent_securite(
-    id_agent_sec varchar(50) primary key,
+    id_agent_sec int(10) primary key auto_increment,
     grade varchar(50),
     experience varchar(50),
     heures_de_services time );
 
 -- Create Table  Etiquette in our Supermarche Information System's
 create table  etiquette (
-    id_Etiquette varchar(50) primary key,
+    id_Etiquette int(10) primary key auto_increment,
     nature_Etiquette varchar(50));
 
 -- Create Table  Casier in our Supermarche Information System's
 create table  casier (
-    id_Casier varchar(50) primary key,
+    id_Casier int(10) primary key auto_increment,
     dimension varchar(50),
     position varchar(50),
     capacite decimal(50));
 
 -- Create Fournisseur  Casier in our Supermarche Information System's
 create table  fournisseur (
-    id_Fournisseur varchar(50) primary key,
+    id_Fournisseur int(10) primary key auto_increment,
     linea varchar(50),
     agrement varchar(50),
     registre_de_commerce varchar(50));
 
 -- Create Table  Client in our Supermarche Information System's
 create table  client (
-    id_Client varchar(50) primary key);
+    id_Client int(10) primary key auto_increment);
 
 -- Create Table Rayonniste  Cas in our Supermarche Information System's
-create table  royonniste (
-    id_royonniste varchar(50) primary key,
+create table  rayonniste (
+    id_rayonniste int(10) primary key auto_increment,
     experience varchar(50));
 
 -- Create Table Local  in our Supermarche Information System's
-create table local  (id_local varchar(50) primary key,
+create table local  (
+    id_local int(10) primary key auto_increment,
     nom_local varchar(50),
     superficie_local decimal(50));
 
 -- Create Table  Rayon in our Supermarche Information System's
 create table  rayon (
-    id_rayon varchar(50) primary key,
+    id_rayon int(10) primary key auto_increment,
     emplacement varchar(50),
     longueur decimal(50),
     nombre_de_casier int(100),
@@ -94,11 +94,11 @@ create table  rayon (
 
 -- Create Table  Caisse in our Supermarche Information System's
 create table caisse   (
-    id_caisse varchar(50) primary key);
+    id_caisse int(10) primary key auto_increment);
 
 -- Create Table Adresse  in our Supermarche Information System's
 create table adresse  (
-    id_adresse varchar(50) primary key,
+    id_adresse int(10) primary key auto_increment,
     region  varchar(50),
     ville varchar(50),
     code_postal varchar(50),
@@ -106,125 +106,138 @@ create table adresse  (
 
 -- Create Table Livreur in our Supermarche Information System's
 create table livreur  (
-    id_livreur varchar(50) primary key,
+    id_livreur int(10) primary key auto_increment,
     nature_permis varchar(50),
     moyen_de_transport varchar(50));
 
 -- Create Table Facture in our Supermarche Information System's
 create table facture (
-    id_facture varchar(50) primary key,
+    id_facture int(10) primary key auto_increment,
     date_facture date,
-    montant_facture decimal(10,2)
-    montant_donne decimal(10,2)
+    montant_facture decimal(10,2),
+    montant_donne decimal(10,2),
     montant_de_retour decimal(10,2));
 
 -- Create Table Avoirs in our Supermarche Information System's
 create table avoirs (
-    id_avoirs varchar(50) primary key,
+    id_avoirs int(10) primary key auto_increment,
     montant_avoirs decimal(10,2),
     date_avoirs date,
     duree_de_validite time);
 
 -- Create Table Commande in our Supermarche Information System's
 create table commande (
-    id_commande varchar(50) primary key);
+    id_commande int(10) primary key auto_increment);
 
 -- Create Table Categories in our Supermarche Information System's
-create table categories (
-    id_categorie varchar(50) primary key);
+create table categorie (
+    id_categorie int(10) primary key auto_increment);
 
 -- Create Table Technicien de surface in our Supermarche Information System's
 create table Tech_surface (
-    id_tech_surface varchar(50) primary key,
+    id_tech_surface int(10) primary key auto_increment,
     nb_heures_services time);
 
 -- Create Table Caissier in our Supermarche Information System's
 create table Caissier (
-    id_caissier varchar(50) primary key,
+    id_caissier int(10) primary key auto_increment,
     experience varchar(50));
 
 -- Create Table Type_client in our Supermarche Information System's
 create table Type_client (
-    id_type_cleint varchar(50) primary key,
+    id_type_client int(10) primary key auto_increment,
     type_client varchar(50));
 
 -------------------------------INSERTION DES CLE ETRANGERES-----------------------------
 
 --Ajouter un attribut (cle etrangere) dans une table
-alter table article add id_casier varchar(50) not null after id_article;
+alter table article add id_casier int(10) not null after id_article;
 
 --Definir l'attribut etrangere comme cle etrangere
-alter table article add constraint FK_id_caisier foreign key(id_casier) references casier(id_casier);
+alter table article add constraint FK_id_caisier_of_article foreign key(id_casier) references casier(id_casier);
 
 --Ajouter un attribut (cle etrangere) dans une table
-alter table commande add id_client varchar(50) not null after id_commande;
+alter table commande add id_client int(10) not null after id_commande;
 
 --Definir l'attribut etrangere comme cle etrangere
-alter table commande add constraint FK_id_client foreign key (id_client) references client(id_client);
+alter table commande add constraint FK_id_client_of_commande foreign key (id_client) references client(id_client);
 
 --Ajouter un attribut (cle etrangere) dans une table
-alter table commande add id_liveur varchar(50) not null after id_commande;
+alter table commande add id_livreur int(10)not null after id_commande;
 
 --Definir l'attribut etrangere comme cle etrangere
-alter table commande add constraint FK_id_livreur foreign key (id_livreur) references livreur(id_livreur);
+alter table commande add constraint FK_id_livreur_of_commande foreign key (id_livreur) references livreur(id_livreur);
 
 --Ajouter un attribut (cle etrangere) dans une table
-alter table categories add id_article varchar(50) NOT NULL after id_categorie;
+alter table categorie add id_article int(10) NOT NULL after id_categorie;
 
 --Definir l'attribut etrangere comme cle etrangere
-alter table categories add constraint FK_id_article foreign key(id_article) references article(id_article);
+alter table categorie add constraint FK_id_article_of_categorie foreign key(id_article) references article(id_article);
 
 --Ajouter un attribut (cle etrangere) dans une table
-alter table rayon add id_rayonniste varchar(50);
+alter table rayon add id_rayonniste int(10) not null after id_rayon;
 
 --Definir l'attribut etrangere comme cle etrangere
-alter table rayon add constraint FK_id_rayonniste foreign key(id_rayonniste) references rayonniste(id_rayonniste);
+alter table rayon add constraint FK_id_rayonniste_of_rayon foreign key(id_rayonniste) references rayonniste(id_rayonniste);
 
 --Ajouter un attribut (cle etrangere) dans une table
-alter table rayon add id_local varchar(50);
+alter table rayon add id_local int(10) not null after id_rayon;
 
 --Definir l'attribut etrangere comme cle etrangere
-alter table rayon add constraint FK_id_local foreign key(id_local) references local(id_local);
+alter table rayon add constraint FK_id_local_of_rayon foreign key(id_local) references local(id_local);
 
 --Ajouter un attribut (cle etrangere) dans une table
-alter table agent_securite add id_local varchar(50) after id_agent_sec;
+alter table agent_securite add id_local int(10)  not null after id_agent_sec;
 
 --Definir l'attribut etrangere comme cle etrangere
-alter table agent_securite add constraint FK_id_local foreign key(id_local) references local(id_local);
+alter table agent_securite add constraint FK_id_local_of_agent_securite foreign key(id_local) references local(id_local);
 
 --Ajouter un attribut (cle etrangere) dans une table
-alter table tech_surface add id_local varchar(50) after id_tech_surface;
+alter table tech_surface add id_local int(10)  not null after id_tech_surface;
 
 --Definir l'attribut etrangere comme cle etrangere
 alter table tech_surface add constraint FK_id_local_of_tech_surf foreign key(id_local) references local(id_local);
 
 --Ajouter un attribut (cle etrangere) dans une table
-alter table gerant add id_local varchar(50) not null after id_gerant;
+alter table gerant add id_local int(10) not null after id_gerant;
 
 --Definir l'attribut etrangere comme cle etrangere
 alter table gerant add constraint FK_id_local_of_gerant foreign key(id_local) references local(id_local);
 
 --Ajouter un attribut (cle etrangere) dans une table
-alter table caisse add id_local varchar(50) not null after id_caisse;
+alter table caisse add id_local int(10) not null after id_caisse;
 
 --Definir l'attribut etrangere comme cle etrangere
 alter table caisse add constraint FK_id_local_of_caisse foreign key(id_local) references local(id_local);
 
 --Ajouter un attribut (cle etrangere) dans une table
-alter table caissier add id_caisse varchar(50) not null after id_caissier;
+alter table caissier add id_caisse int(10) not null after id_caissier;
 
 --Definir l'attribut etrangere comme cle etrangere
 alter table caissier add constraint FK_id_caisse_of_caissier foreign key(id_caisse) references caisse(id_caisse);
 
 --Ajouter un attribut (cle etrangere) dans une table
-alter table type_client add id_client varchar(50) not null after id_type_client;
+alter table type_client add id_client int(10) not null after id_type_client;
 
 --Definir l'attribut etrangere comme cle etrangere
 alter table type_client add constraint FK_id_client_of_type_client foreign key(id_client) references client(id_client);
 
+--Ajouter un attribut (cle etrangere) dans une table
+alter table personne add id_adresse int(10) not null after id_personne;
+
+--Definir l'attribut etrangere comme cle etrangere
 alter table personne add constraint FK_id_adresse_of_personne foreign key(id_adresse) references adresse(id_adresse);
 
+--
+--Ajouter un attribut (cle etrangere) dans une table
+alter table local add id_adresse int(10) not null after id_local;
 
+--Definir l'attribut etrangere comme cle etrangere
+alter table local add constraint FK_id_adresse_of_local foreign key(id_adresse) references adresse(id_adresse);
+
+-----------------------------Changer le type des cle plus auto increment-------------------------------
+
+ alter table personne modify id_personne int(10) not null auto_increment;
 
 
 -----------------------------CHANGER LES NOM DES ENTITE ET  DES ATTRIBUTS-------------------------------
@@ -287,8 +300,14 @@ ADD CONSTRAINT CHK_maTable_maColonne
 CHECK(maColonne [formuleDeVerification]);
 
 
+--supprimer une cle etrangere
+ ALTER TABLE personne DROP constraint FK_id_adresse_of_personne;
+ alter table personne Drop id_adresse;
+
+
 -----------------------------INSERTION DES DONNEES-------------------------------
 
 --Table personne
 insert into personne (id_adresse,nom,prenom_pers,mail,telephone,age) values ('jarvis', 'Stark', 'jarvis@gmail.com', '77 777 77 77', '22');
 drop 
+delete from personne where nom='jarvis';
